6d8c327ed3a4863a93bad4f26831bbe82a9735a9
Bui Tuan Dat <tuandat092905@gmail.com>
2023-09-28 22:42:53 +0700

    chapter 2, video 6

--- video3/video3.js
+++ video3/video3.js
@@ -4,4 +4,4 @@
     console.log(`hello ''" ' world from javascript: `)
     let x = 5, y = 6, z = 7;
     console.log(" Test string", 'x= ', x, ' y= ', y, ' z=', z)
-    console.log(" Test string", 'x= ', x, 'y= ', y, 'z= ', z)
+    console.log(" Test string", 'x= ', x, 'y= ', y, 'z= ', z) 
