d16bfb08f1f961e87ad0e9a89d7e41fd43243399
Bui Tuan Dat <tuandat092905@gmail.com>
2023-10-01 00:58:35 +0700

    chapter 3, video 11

---
+++ video11/video11.js
@@ -0,0 +1,10 @@
+console.log('Hello world from HTML')
+
+let arr = ['Mu', 'Mancity', 'Liver', 'Chelse']
+let i = 0;
+for(let i = 0; i < arr.length; i++){
+    console.log('check variable i = ', arr[i])
+
+}
+
+console.log('check value i: ', i)
